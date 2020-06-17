7a031b33931ed8bc9645e4df9a9fd24c6ddc9f73
CapSmollet <muaxim@gmail.com>
2020-06-17 18:34:13 +0300

    Changes to be committed: 	new file:   task1/Main.cpp 	modified:   Main.cpp 	modified:   _vizdoom.ini 	deleted:    task2/Main.cpp

---
+++ task2/Main.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <ViZDoom.h>
+#include <chrono>
+#include <thread>
+
+vizdoom::DoomGame* game = new vizdoom::DoomGame();
+auto screenBuff = cv::Mat(480, 640, CV_8UC3);
+const unsigned int sleepTime = 1000 / vizdoom::DEFAULT_TICRATE;
+double sum = 0;
+
+/*void game_init() {
+	game->setScreenResolution(vizdoom::RES_640X480);
+	game->setLabelsBufferEnabled(true); // add this
+	game->setWindowVisible(true);
+	game->setRenderWeapon(true);
+	game->setRenderHud(true);
+}*/
+
+void RunTask1(int episodes)
+{
+	try
+	{
+		game->loadConfig("../vizdoom/scenarios/task1.cfg");
+		game->init();
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	auto greyscale = cv::Mat(480, 640, CV_8UC1);
+
+	std::vector<double> action;
+
+	unsigned int sleepTime = 2000 / vizdoom::DEFAULT_TICRATE;
+	for (auto i = 0; i < episodes; i++)
+	{
+		game->newEpisode();
+		std::cout << "Episode #" << i + 1 << std::endl;
+		int x = 0;
+		while (!game->isEpisodeFinished())
+		{
+			const auto& gamestate = game->getState();
+
+			std::memcpy(screenBuff.data, gamestate->screenBuffer->data(), gamestate->screenBuffer->size());
+
+			cv::extractChannel(screenBuff, greyscale, 2);
+
+			cv:threshold(greyscale, greyscale, 175, 255, cv::THRESH_BINARY);
+
+			for (int c = 0; c < 640; c++) 
+			{
+				if ((int)greyscale.at<uchar>(cv::Point(c, 240)) == 255)
+				{
+					x = c;
+					//std::cout << x << " ";
+					//game->makeAction({ 1, 0, 0});
+				}
+			}
+			if (x <= 320)
+			{
+				game->makeAction({ 1, 0, 0 });
+			}
+			else
+			{
+				//std::cout << x << '\n';
+				game->makeAction({ 0, 1, 0 });
+			}
+			if ((int)greyscale.at<uchar>(cv::Point(320, 240)) == 255)
+			{
+				double reward = game->makeAction({ 0, 0, 1 });
+			}
+			cv::imshow("Output Window", greyscale);
+			//double reward = game->makeAction({ 1 });
+
+			//std::cout << reward << " ";
+			cv::waitKey(sleepTime);
+		}
+
+		sum += game->getTotalReward();
+		std::cout << std::endl << game->getTotalReward() << std::endl;
+
+	}
+}
+
+int main() {
+	game->setViZDoomPath("../vizdoom/vizdoom");
+	game->setDoomGamePath("../vizdoom/freedoom2.wad");
+
+	cv::namedWindow("Output Window", cv::WINDOW_AUTOSIZE);
+
+	auto episodes = 10;
+
+	//------------------
+	RunTask1(episodes);
+	//------------------
+
+	std::cout << sum / 10;
+
+	game->close();
+	delete game;
+}
