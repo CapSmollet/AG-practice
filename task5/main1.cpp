05b3b1f423a5e930488c540a9c9b08b1c5fb0d9b
stasan320 <44366841+stasan320@users.noreply.github.com>
2020-06-24 13:03:54 +0300

    в принципе работает

--- task5/main1.cpp
+++ task5/main1.cpp
@@ -1,91 +1,101 @@
 #include <iostream>
-#include <Windows.h>
 #include <opencv2/opencv.hpp>
 #include <ViZDoom.h>
-#include <vector>
-#include <cstdlib>
 #include <chrono>
 #include <thread>
 
 vizdoom::DoomGame* game = new vizdoom::DoomGame();
-unsigned int sleepTime = 1000 / vizdoom::DEFAULT_TICRATE;
-std::string path = "C:\\practice\\practice\\vizdoom";
 auto screenBuff = cv::Mat(480, 640, CV_8UC3);
+const unsigned int sleepTime = 1000 / vizdoom::DEFAULT_TICRATE;
+double sum = 0;
+//const float EPS = 10;
 
-void RunTask1(int episodes) {
-	double reward = 0;
-	try {
-		game->loadConfig(path + "\\scenarios\\task5.cfg");
+/*void game_init() {
+	game->setScreenResolution(vizdoom::RES_640X480);
+	game->setLabelsBufferEnabled(true); // add this
+	game->setWindowVisible(true);
+	game->setRenderWeapon(true);
+	game->setRenderHud(true);
+}*/
+
+void RunTask1(int episodes)
+{
+	try
+	{
+		game->loadConfig("../vizdoom/scenarios/task5.cfg");
 		game->init();
 	}
-	catch (std::exception& e) {
+	catch (std::exception& e)
+	{
 		std::cout << e.what() << std::endl;
 	}
 
-	auto greyscale = cv::Mat(480, 640, CV_8UC3);
-	auto left = cv::Mat(480, 640, CV_8UC3);
-	auto right = cv::Mat(480, 640, CV_8UC3);
-	uchar max = 150;
+	auto greyscale = cv::Mat(480, 640, CV_8UC1);
 
+	std::vector<double> action;
 
-	for (auto i = 0; i < episodes; i++) {
-		//std::string filename = "C:\\practice\\practice\\vizdoom\\sprites\\Pickups\\media0.png";
+	unsigned int sleepTime = 2000 / vizdoom::DEFAULT_TICRATE;
+	for (auto i = 0; i < episodes; i++)
+	{
 		game->newEpisode();
-		//std::cout << "Episode #" << i + 1 << std::endl;
-
-		while (!game->isEpisodeFinished()) {
-			int per = 0, run = 0;
+		std::cout << "Episode #" << i + 1 << std::endl;
+		while (!game->isEpisodeFinished())
+		{
 			const auto& gamestate = game->getState();
+
 			std::memcpy(screenBuff.data, gamestate->screenBuffer->data(), gamestate->screenBuffer->size());
 
-			cv::extractChannel(screenBuff, greyscale, 0);
-			cv::threshold(greyscale, greyscale, 150, 255, cv::THRESH_BINARY);
+			cv::extractChannel(screenBuff, greyscale, 2);
 
-			cv::Rect regl(0, 0, 320, 400);
-			greyscale(regl).copyTo(left);
-			cv::Rect reg2(320, 0, 320, 400);
-			greyscale(reg2).copyTo(right);
-			cv::Rect reg(0, 0, 640, 400);
-			greyscale(reg).copyTo(greyscale);
+		cv:threshold(greyscale, greyscale, 200, 255, cv::THRESH_BINARY);
 
-
-			for (int i = 0; i < greyscale.rows; i++) {
-				for (int j = 0; j < greyscale.cols; j++) {
-					if (greyscale.at<uchar>(i, j) == 255) run = 1;
-				}
-			}*/
-
-			/*for (int i = 0; i < left.rows; i++) {
-				for (int j = 0; j < left.cols; j++) {
-					if (left.at<uchar>(i, j) == 255) per = 1;
+			int found = 0;
+			for (int i = 0; i < 640; i++)
+			{
+				if ((int)greyscale.at<uchar>(cv::Point(i, 220)) == 255)
+				{
+					if (i <= 320)
+					{
+						game->makeAction({ 0, 1 });
+						break;
+					}
+					if (i > 320)
+					{
+						game->makeAction({ 1, 0 });
+						break;
+					}
+					found = 1;
+					break;
 				}
 			}
-
-			//if (run == 1) game->makeAction({ 0, 0, 1 });
-			if (per == 1) game->makeAction({ 1, 0 });
-			else game->makeAction({ 0, 1 });
-			//std::cout << per << std::endl;
-
-			cv::imshow("Left", left);
-			cv::imshow("Right", right);
-			cv::imshow("Result", greyscale);
+			if (found == 0)
+			{
+				game->makeAction({ 0, 0 });
+			}
+			cv::imshow("Output Window", greyscale);
 			cv::waitKey(1);
 		}
 
+		sum += game->getTotalReward();
 		std::cout << std::endl << game->getTotalReward() << std::endl;
-		reward = reward + game->getTotalReward();
+
 	}
-	std::cout << "TotalRew " << reward / episodes;
 }
 
 int main() {
-	game->setViZDoomPath(path + "\\vizdoom.exe");
-	game->setDoomGamePath(path + "\\freedoom2.wad");
+	game->setViZDoomPath("../vizdoom/vizdoom");
+	game->setDoomGamePath("../vizdoom/freedoom2.wad");
 
-	//cv::namedWindow("Output", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("Output Window", cv::WINDOW_AUTOSIZE);
 
 	auto episodes = 10;
+
+	//------------------
 	RunTask1(episodes);
+	//------------------
+
+	std::cout << sum / 10;
 
 	game->close();
+	delete game;
 }
